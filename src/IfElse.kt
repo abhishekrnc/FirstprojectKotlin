525a758e996a2335f4a3d6941083e45c9efbdbbb
Abhishek <abhishektupudana@gmail.com>
2020-08-28 00:40:26 +0530

    Did More Changes

--- src/IfElse.kt
+++ src/IfElse.kt
@@ -1,4 +1,45 @@
 fun main() {
-    val isRaining = true
+   // val isRaining = true
 
+   // if(isRaining == true) {
+     //   println("Please take an Umbrella")
+   // }
+
+   //else {
+     //   println("No need of an Umbrelal today")
+    // }
+
+
+
+    //val x = 1
+    //val y = 10
+
+
+    //if(x > y) {
+    //    println("X is greater than Y")
+    //}
+
+   // else if(x < y) {
+    //    println ("X is less than Y")
+    //}
+   // else {
+    //    println("X is equal to y")
+  //  }
+
+
+    //If Else Even Odd
+
+    val number = 20
+    val result = if(number % 2 == 0) {
+        "The number is even"
+    }
+    else {
+        "The number is odd"
+    }
+    println(result)
+
+    //Or The other way to write the above statement is
+
+    val result1 = if (number % 2 == 0) "Even" else "odd"
+    print(result1)
 }
