7b648e5cd7f5e950328627456fa9e741c74a970f
Abhishek <abhishektupudana@gmail.com>
2020-08-28 01:16:24 +0530

    Did More ChangKes

--- src/Range.kt
+++ src/Range.kt
@@ -1,5 +1,5 @@
 fun main() {
-    val number = 5
-    val result = number in 1..5  //1,2,3,4,5
+    val number = 7
+    val result = number in 1..5  //1,2,3,4,5   // 1 until 5 for onyl 1,2,3,4
     print(result)
 }
