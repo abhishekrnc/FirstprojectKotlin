5b58a6865711f22bbabeaf58d8ffdffc72e19d30
Abhishek <abhishektupudana@gmail.com>
2020-08-27 23:33:55 +0530

    More Changes

--- src/Operators.kt
+++ src/Operators.kt
@@ -1,9 +1,57 @@
 fun main() {
-   var i = 13
-    var j = 2
+   //var i = 13
+   //var j = 2
 
-    println(i + i)  //15
-    println (i - j)  //11
-    println (i * j)  //26
-    println (i .toFloat()/ j)  //6.5
+
+ //Mathematical operators
+   // println(i + i)  //15
+   // println (i - j)  //11
+   // println (i * j)  //26
+   // println (i .toFloat()/ j)  //6.5
+   // println (i % j)
+
+
+ //Logical Operators
+  //println(i > j) // 13 > 2 true
+  //println(i < j) // 13 < 2 false
+  //println( i >= j)  // 13 >= 2 true
+  //println( i <= j) // 13 <==2 false
+  //println( i == j) // 13 == 2 false
+  //println( i != j) // 13 != 2 true
+
+
+//Pre & Post increment
+ // var a = 10
+ // println(a++)
+ // println(++a)
+ // println(a++)
+
+//val above70 = false
+//val knowsProgramming = false
+
+ //&& AND
+ //var calledForInterview = above70 && knowsProgramming
+ //println(calledForInterview)
+
+ //|| OR
+ //calledForInterview = above70 || knowsProgramming
+ //println(calledForInterview)
+
+
+ //Short Circuiting
+
+ //var i = 10
+ //var j = 11
+
+ //var result = i == 10 || j++ == 11
+ //println(i)
+ //println(j)
+
+
+ //Not Operator
+
+ val answer = false
+ val result = !answer
+ println(result)
+
 }
