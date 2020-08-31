3a4ecaf45f0090cbc78cbdf230dc2d4ff3c74fc8
Abhishek <abhishektupudana@gmail.com>
2020-08-31 23:22:28 +0530

    functions

--- src/Functions.kt
+++ src/Functions.kt
@@ -2,11 +2,16 @@ fun main() {
     val result = add(4, 5)   //Function calling and this is the second step
     println (result)
     val result2 = add(5, 10)
-
+    println(result2)
 }
 
 fun add(num1 : Int, num2 : Int): Int
 {                                                   //This is the first step and here we have defined the function
     val sum = num1 + num2
     return sum
+}
+
+fun evenOrOdd(num1 : Int) {
+    val result = if (num1 % 2 == 0) "Even" else "odd"
+    println(result)
 }
