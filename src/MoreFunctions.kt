6127438d7d2b47549098f401ed14f1c99b1bb4cc
Abhishek <abhishektupudana@gmail.com>
2020-09-02 22:43:59 +0530

    function overloading

--- src/MoreFunctions.kt
+++ src/MoreFunctions.kt
@@ -1,14 +1,19 @@
 fun main() {
-    println(addition(1,3))
-    println(addition(1.0, 3.0))
+   // println(addition(1,3))
+   // println(addition(1.0, 3.0))
+   // println(addition(a = 2, b = 6))         //Named Argument concept in Kotlin
+
+    var fn = ::addition                  //Storing value in varible
+    println(fn(1.4, 5.6))
 }
 
-fun addition(a : Int, b: Int): Int
-{
-    return a + b
-}
+//fun addition(a : Int, b: Int): Int
+//{
+//    return a + b
+//}
 
 fun addition(a : Double, b: Double): Double
 {
     return a + b
 }
+
