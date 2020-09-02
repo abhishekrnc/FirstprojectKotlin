06d47f7db60eb7fc69dc3c4b84e65966cf3f46b6
Abhishek <abhishektupudana@gmail.com>
2020-09-02 22:53:51 +0530

    function overloading

--- src/MoreFunctions.kt
+++ src/MoreFunctions.kt
@@ -1,3 +1,5 @@
+import kotlin.math.pow
+
 fun main() {
    // println(addition(1,3))
    // println(addition(1.0, 3.0))
@@ -5,6 +7,10 @@ fun main() {
 
     var fn = ::addition                  //Storing value in varible
     println(fn(1.4, 5.6))
+
+
+    fn = ::power
+    println(fn(2.0, 3.0))
 }
 
 //fun addition(a : Int, b: Int): Int
@@ -17,3 +23,7 @@ fun addition(a : Double, b: Double): Double
     return a + b
 }
 
+
+fun power(a:Double, b: Double):Double {
+    return a.pow(b)
+}
