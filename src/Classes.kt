66cd4780777d7f228500aa5792046af594837ddf
Abhishek <abhishektupudana@gmail.com>
2020-09-04 03:25:12 +0530

    Classes

--- src/Classes.kt
+++ src/Classes.kt
@@ -3,9 +3,13 @@ fun main() {
 
 }
 
-class Car (val name : String, val type : String, var kmRan : Int)                  //Class declaration in Kotlin
+class Car (val name : String, val type : String, var kmRan : Int)                  //Class declaration in Kotlin   //Properties
 {
     fun drive(){                                //Method declaration in Kotlin
 
+    }
+
+    fun applyBrakes(){
+        println
     }
 }
