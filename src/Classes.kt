d6eeba73353334836a4662d552320569586d80de
Abhishek <abhishektupudana@gmail.com>
2020-09-04 03:34:15 +0530

    Classes

--- src/Classes.kt
+++ src/Classes.kt
@@ -1,15 +1,26 @@
 fun main() {
+    val mustang = Car("mustang", "petrol",  100)            //Creating Objects
+    val beetle = Car( "beetle",  "diesel",  200)
 
+    println(mustang.name)               //calling the properties
+    println(mustang.type)
+
+    println(beetle.name)
+
+
+    mustang.driveCar()
 
 }
 
 class Car (val name : String, val type : String, var kmRan : Int)                  //Class declaration in Kotlin   //Properties
 {
-    fun drive(){                                //Method declaration in Kotlin
-
+    fun driveCar()
+    {                                //Method declaration in Kotlin
+        println("Car is driving")
     }
 
-    fun applyBrakes(){
-        println
+    fun applyBrakes()
+    {
+        println("Applied Brakes")
     }
 }
