817ede1bce8d53ae96320196d486f6e5cb9f3483
Abhishek <abhishektupudana@gmail.com>
2020-09-04 23:16:18 +0530

    Classes

---
+++ src/resturant.kt
@@ -0,0 +1,17 @@
+fun main() {
+    //var Chicken_curry : String = "Chicken Curry"                                  // Menu ( Chicken, Fish curry, Biryani)
+    //var Fish_curry : String = "Fish Curry"
+   // var Biryani : String = "Biryani"
+
+   // var chicken_curry : Int = 120
+
+    val Chicken = menu ("Chicken Curry" , 180)
+    val fish = menu ("Fish Curry" , 170)
+    val biryani = menu ("Biryani" , 320)
+
+}                                                                                        // A Resturant
+    class menu (val name : String, var price : Int)
+    {
+
+    }
+
